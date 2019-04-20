58bb8eb2400189f52b788c5c7776c3ade5b0f05b
TorkyBlorky <cameroncorbett.gaming1@gmail.com>
2019-04-20 09:55:06 +0100

    ProcessingUpdate

--- enigma.js
+++ enigma.js
@@ -1,7 +1,10 @@
-function setup() {
-  createCanvas(600,600);
-}
-
-function draw() {
-  background(51);
-}
+(function(processing, $constants) {
+    function setup() {
+        processing.size(200, 200);
+        processing.background(100);
+        processing.stroke(255);
+        processing.ellipse(50, 50, 25, 25);
+        processing.println("hello web!");
+    }
+    processing.setup = setup;
+})
